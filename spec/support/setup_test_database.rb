b568281f18e8c73190b9223e39c1ba0bcbfe5f3b
Patryk Okpara <patrykokpara@hotmail.com>
2022-03-15 17:40:31 +0000

    Wrote a helper method that empties the bookmarks table before each run

---
+++ spec/support/setup_test_database.rb
@@ -0,0 +1,16 @@
+require 'pg'
+
+def setup_test_database
+  connection = PG.connect(dbname: 'bookmark_manager_test')
+
+  connection.exec("DROP TABLE bookmarks;")
+  connection.exec("CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, title VARCHAR(60), url VARCHAR(60));")
+end
+
+def add_default_bookmarks_to_test_database
+  connection = PG.connect(dbname: 'bookmark_manager_test')
+  
+  connection.exec("INSERT INTO bookmarks (title, url) VALUES ('The Internet 4', 'http://www.what-more-internet.com/');")
+  connection.exec("INSERT INTO bookmarks (title, url) VALUES ('The Internet 3', 'http://www.still-internet.com/');")
+  connection.exec("INSERT INTO bookmarks (title, url) VALUES ('The Internet', 'http://www.internet.com/');")
+end
